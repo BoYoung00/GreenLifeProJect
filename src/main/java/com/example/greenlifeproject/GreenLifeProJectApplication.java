edd3ead2aae64c6ede1b6f06636964dc126d70ac
JOODON <launcher37@naver.com>
2023-06-22 16:22:00 +0900

    Initial commit

---
+++ src/main/java/com/example/greenlifeproject/GreenLifeProJectApplication.java
@@ -0,0 +1,13 @@
+package com.example.greenlifeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenLifeProJectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenLifeProJectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/greenlifeproject/GreenLifeProJectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greenlifeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenLifeProJectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
